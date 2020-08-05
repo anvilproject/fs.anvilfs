3b36ae3b3923d61e3dc93616ee865676a3251819
Luke Sargent <luke.c.sargent@gmail.com>
2020-08-05 15:51:12 -0700

    upload working

--- anvilfs/anvilfs.py
+++ anvilfs/anvilfs.py
@@ -19,14 +19,19 @@ root: Workspace
 
 """
 
-import firecloud.api as fapi
+# std
 import threading
-
+from io import BytesIO
+#firecloud
+import firecloud.api as fapi
+# pyfilesystem2
 from fs.base import FS
 from fs.enums import ResourceType
 from fs.errors import DirectoryExpected, ResourceNotFound, FileExpected
 from fs.info import Info
+# google
 from google.cloud import storage
+
 
 
 """
@@ -97,15 +102,18 @@ class Workspace(BaseAnVILResource):
         self.lastModified = resp["workspace"]["lastModified"]
 
     def get_object_from_path(self, path):
-        assert(path[0] == "/")
+        print("gofp: {}".format(path))
         if path == "/":
             return self.folders
         idx = path.find("/", 1)
+        print("gofpi: {}".format(idx))
         # if the next slash is the last slash, its a directory
-        if idx == len(path) - 1:
+        if idx == len(path) - 1 or idx < 0:
             return self.folders[path[1:]]
+        print("gofp2: {}".format(path))
         first_subfolder = path[1:idx+1]
         remainder = path[idx:]
+        print("{} - {} -".format(first_subfolder, remainder))
         return self.folders[first_subfolder].get_object_from_path(remainder)
 
     def get_info_from_path(self, path):
@@ -120,11 +128,21 @@ class Workspace(BaseAnVILResource):
 
 # google stores bucket files as a/b/c.extension;
 #   so file dictionary stores { "a/b/":["c.extension", ...]}
-class BucketFile(BaseAnVILResource):
-    def __init__(self, name, size):
-        self.name = name
-        self.size = size
+class BucketFile(BaseAnVILResource):#, BytesIO):
+    def __init__(self, blob):
+        # super().__init__()
+        self.name = blob.name
+        self.size = blob.size
+        self.blob_handle = blob
         self.is_dir = False
+
+    def get_bytes_handler(self):
+        print("get_bytes_handler getting called for {}".format(self.name))
+        buffer = BytesIO()
+        self.blob_handle.download_to_file(buffer)
+        buffer.seek(0)
+        return buffer
+
 
 class WorkspaceBucket(BaseAnVILResource):
     def __init__(self, bucket_name):
@@ -136,7 +154,7 @@ class WorkspaceBucket(BaseAnVILResource):
         #NOTE can generate signed urls from blobs with 'blob.generate_signed_url'
         #NOTE blobs are never just folders
         for blob in blobs:
-            self.insert_file(blob.name, blob.size)
+            self.insert_file(blob)
 
     def __getitem__(self, item):
         return self.folders[item]
@@ -144,12 +162,14 @@ class WorkspaceBucket(BaseAnVILResource):
     def keys(self):
         return self.folders.keys()
 
-    def insert_file(self, bucket_file, file_size):
-        print("inserting {}".format(bucket_file))
+    def insert_file(self, bucket_blob):
+        bucket_file = bucket_blob.name
+        file_size = bucket_blob.size
+        print("inserting: {}".format(bucket_file))
         idx = bucket_file.find('/')
         # if there is no '/' its a file
         if idx < 0:
-            self.folders[bucket_file] = BucketFile(bucket_file, file_size)
+            self.folders[bucket_file] = BucketFile(bucket_blob)
         else:
             current_level = self.folders
             split = bucket_file.split("/")
@@ -160,10 +180,9 @@ class WorkspaceBucket(BaseAnVILResource):
                 if level not in current_level:
                     current_level[level] = {}
                 current_level = current_level[level]
-            current_level[file_name] = BucketFile(file_name, file_size)
+            current_level[file_name] = BucketFile(bucket_blob)
 
     def get_object_from_path(self, path):
-        assert(path[0] == "/")
         if path == "/":
             return self.folders
         levels = path.split("/")[1:]
@@ -191,36 +210,83 @@ class Cohort(BaseAnVILResource):
 
 # READ ONLY filesystem?
 class AnVILFS(FS):
+
+    def leading_slash(f):
+        def wrapped(self, path, *args, **kwargs):
+            if "path" in kwargs:
+                if kwargs["path"][0] != "/":
+                    kwargs["path"] = "/" + kwargs["path"]
+            elif isinstance(path, str):
+                if path[0] != "/":
+                    path = "/" + path
+            return f(self, path, *args, **kwargs)
+        return wrapped
+
     # input = name strings
     def __init__(self, namespace, workspace):
         super(AnVILFS, self).__init__()
-        #self._lock = threading.RLock()
+        self._lock = threading.RLock()
         self.namespace = Namespace(namespace)
         self.namespace.fetch_workspace(workspace)
         self.workspace = self.namespace.folders[workspace]
 
     # this is all relative to the workspace, might need to be extended to namespace?
+    @leading_slash
     def getinfo(self, path, namespaces=None):
+        print("afs: getinfo({})".format(path))
         return self.workspace.get_info_from_path(path)
 
-# required functions #TODO
+    @leading_slash
     def listdir(self, path):# Get a list of resources in a directory.
+        if path[-1] != "/":
+            path = path + "/"
+        print("afs: listdir({})".format(path))
         try:
             maybe_dir = self.workspace.get_object_from_path(path)
+            print("md: {}".format(maybe_dir))
         except KeyError as ke:
             raise ResourceNotFound("Resource {} not found".format(path))
         if isinstance(maybe_dir, dict) or maybe_dir.is_dir:
-            return maybe_dir.keys()
+            print("returning dir keys {}".format(maybe_dir.keys()))
+            return list(maybe_dir.keys())
         else:
             raise DirectoryExpected("{} is not a directory".format(path))
+
+    @leading_slash
+    def scandir(self, path):
+        if path[-1] != "/":
+            path = path + "/"
+        print("afs: scandir({})".format(path))
+        result = []
+        l = self.listdir(path)
+        print("listdir list: {}".format(l))
+        for o in l:
+            print(path+o)
+            result.append(self.getinfo(path+o))
+        return result
+
     def makedir():# Make a directory.
-        pass
+        raise Exception("makedir not implemented")
+    
+    @leading_slash
     def openbin(self, path, mode="r", buffering=-1, **options):
-        pass
+        print("afs: openbin({})".format(path))
+        obj = self.workspace.get_object_from_path(path)
+        try:
+            return obj.get_bytes_handler()
+        except AttributeError as e:
+            raise FileExpected("Error: requested object is not a file:\n  {}".format(path))
+
+    # @leading_slash
+    # def download(self, path, *args, **kwargs):
+    #     print("download being called on {}".format(path))
+    #     obj = self.workspace.get_object_from_path(path)
+    #     return obj.download()
+
     def remove():# Remove a file.
-        pass
+        raise Exception("remove not implemented")
     def removedir():# Remove a directory.
-        pass
+        raise Exception("removedir not implemented")
     def setinfo():# Set resource information.
-        pass
+        raise Exception("setinfo not implemented")
     # for network systems, scandir needed otherwise default calls a combination of listdir and getinfo for each file.
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fs.anvilfs',
+      version='0.0',
+      packages=setuptools.find_packages(),
+      description='PyFilesystem2 AnVIL plugin')
