0e44fe9ec0b375b6808a7f745614f5635fe0c30f
Luke Sargent <luke.c.sargent@gmail.com>
2020-08-20 10:18:27 -0700

    adding required dependencies i should have added previously

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(name='fs.anvilfs',
       version='0.0',
       packages=setuptools.find_packages(),
       install_requires=[
-            'firecloud>=0.16.28'
+            'firecloud>=0.16.28',
+            'google-cloud-storage',
+            'fs'
       ],
       description='PyFilesystem2 AnVIL plugin')
